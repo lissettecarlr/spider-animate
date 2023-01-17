44cebcc808b347e45a35a28129741c94bad69d56
lissettecarlr <lissettecarlr@163.com>
2023-01-17 09:21:24 +0800

    add animate

--- server/addSearchAinmate.py
+++ server/addSearchAinmate.py
@@ -3,10 +3,22 @@ from config.setting import settinInit,addNewAnimate
 if __name__ == "__main__":
     settinInit()
     # addNewAnimate("填入名称","其他关键词") 以下为示例
-    addNewAnimate("冰海战记 S02","1080")
+    # 2023年一月  
+    # 除了尼尔和万事屋其他都看过漫画或者小说
+    addNewAnimate("冰海战记 S02","1080") # 
     addNewAnimate("在地下城尋求邂逅是否搞錯了什麼 S04","1080")
     addNewAnimate("間諜教室","1080 喵萌奶茶屋")
     addNewAnimate("生而为狗 我很幸福","1080")
     addNewAnimate("NieR Automata","1080")
     addNewAnimate("虚构推理 第二季","1080")
- 
+    addNewAnimate("被解雇的暗黑士兵","1080 LoliHouse") 
+    addNewAnimate("万事屋斋藤","1080 喵萌奶茶屋")
+    addNewAnimate("妖幻三重奏","1080")
+    addNewAnimate("不要欺负我 2nd","1080")
+    addNewAnimate("久保同学不放过我","1080")
+    # 2022年遗留
+    addNewAnimate("异世界舅舅","1080")
+    addNewAnimate("想要成为影之实力者","1080 简中")
+
+
+  
